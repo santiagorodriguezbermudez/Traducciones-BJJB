d14f4015f4713d56af7aabb899b2b06341f5b98c
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-07-04 12:34:22 -0500

    Added new client's logos

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -9,14 +9,14 @@ import Row from 'react-bootstrap/Row';
 function About (props) {
     return (
         <Container id={props.id}>
-                <h2>Nuestros Clientes</h2>
+                <h2>NUESTROS CLIENTES</h2>
                 <h4>Proyectos Completados: 
                     <CountUp
                         end={873}
                         duration={25}
                     />
                 </h4>
-                <p>Nuestros clientes son tanto empresas como personas naturales, privados como públicos, compañías locales y multinacionales en varios sectores, tales como ingeniería, tecnoglogía, educación, planeación, medicina, salud, legal, financiero, tecnológico y mercadeo.</p>
+                <p>Nuestros clientes son tanto empresas como personas naturales, privados como públicos, compañías locales como multinacionales y en varios sectores, tales como: ingeniería, tecnología, educación, planeación, medicina, salud, legal, financiero y mercadeo.</p>
             
             <Row>
                 <Col sm={false} md={true} lg={true} className="about-logos-competencia">
@@ -26,7 +26,12 @@ function About (props) {
                     <img className="about-client-logo" src="img/Empresa-4.png" alt="logo-4"/>
                     <img className="about-client-logo" src="img/Empresa-5.png" alt="logo-5"/>
                     <img className="about-client-logo" src="img/Empresa-6.png" alt="logo-6"/>
-                    <img className="about-client-logo" src="img/Empresa-7.png" alt="logo-7"/>                    
+                    <img className="about-client-logo" src="img/Empresa-7.png" alt="logo-7"/>
+                    <img className="about-client-logo" src="img/Empresa-8.jpg" alt="logo-8"/>
+                    <img className="about-client-logo" src="img/Empresa-9.png" alt="logo-9"/>
+                    <img className="about-client-logo" src="img/Empresa-10.png" alt="logo-10"/>
+                    <img className="about-client-logo" src="img/Empresa-11.png" alt="logo-11"/>
+                    <img className="about-client-logo" src="img/Empresa-12.jpg" alt="logo-12"/>
                 </Col>
             </Row>
             <Team id="equipo"/>        
