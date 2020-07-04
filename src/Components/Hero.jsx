d7eff88c93d36468d149beb5dbc893d27b187587
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-07-04 11:50:54 -0500

    Trasnform hero text case

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,7 +10,7 @@ function Hero(props) {
         <div id={props.id}>      
         <h1 className='titular'><strong>Traduce tus</strong></h1>
         <h1 className='titular'><em></em><ReactRotatingText items={arrayText}/></h1>
-        <h3 className='sub-titular'>Construimos puentes de comunicación</h3>
+        <h3 className='sub-titular text-uppercase'>Construimos puentes de comunicación</h3>
         <Button variant="info" href="/contacto" >Cotiza ya!</Button>
         </div>
     )
