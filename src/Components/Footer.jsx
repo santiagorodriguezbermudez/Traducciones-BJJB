897f52797cfa2bcd44b5b378d6792ca3f7ddd08f
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-07-26 14:27:56 -0500

    Fixed order within links of footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer(props) {
         <Container id={props.id}>
             <Row>
                 <Col>
-                    <a  href="mailto:jbermudezc@yahoo.com"><i className="icons far fa-envelope-open fa-2x"></i>jbermudezc@yahoo.com</a>    
+                    <a  href="mailto:jbermudezc@yahoo.com"><i className="icons far fa-envelope-open fa-2x"></i>jbermudezc@yahoo.com</a><br></br>    
                     <a href="https://www.linkedin.com/in/jorge-berm%C3%BAdez-celin/" className = ""><i className="icons fab fa-linkedin fa-2x"></i>BJJB - Traducciones</a>
                     <p>Copyright 2020</p>
                 </Col>
