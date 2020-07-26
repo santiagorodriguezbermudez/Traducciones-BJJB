c7613aeefd5920a4f07cf2a1cb741793347a76cb
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-07-26 14:11:11 -0500

    Added carrousel effect to testimonies

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,13 +4,13 @@ import CountUp from 'react-countup';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-
+import Carousel from 'react-bootstrap/Carousel';
 
 function About (props) {
 
     const testimonios = [
         {
-            texto: "El ingeniero Jorge Bermúdez Celín nos viene colaborando desde junio de 2015 y durante ese tiempo su trabajo se ha caracterizado por ser de alta calidad y siempre a tiempo, atendiendo nuestras exigencias. Los trabajos realizados por el ingeniero Bermúdez varían desde comunicaciones informales hasta documentos y contratos de alta complejidad en sus contenidos.",
+            texto: "\"El ingeniero Jorge Bermúdez Celín nos viene colaborando desde junio de 2015 y durante ese tiempo su trabajo se ha caracterizado por ser de alta calidad y siempre a tiempo, atendiendo nuestras exigencias. Los trabajos realizados por el ingeniero Bermúdez varían desde comunicaciones informales hasta documentos y contratos de alta complejidad en sus contenidos.\"",
             firma: [
                 'Paola Pérez M. ',
                 'Representante Legal',
@@ -18,7 +18,7 @@ function About (props) {
             ]
         },
         {
-            texto: "Mr. Bermúdez has translated for us several technical reports related to ports and transportation planning since 2012. His work is highly dependable and excellent quality.",
+            texto: "\"Mr. Bermúdez has translated for us several technical reports related to ports and transportation planning since 2012. His work is highly dependable and excellent quality.\"",
             firma: [
                 'Bart Winder',
                 'Senior Maritime Consultant',
@@ -26,7 +26,7 @@ function About (props) {
             ]
         },
         {
-            texto: "Jorge Bermúdez Celín ha realizado para nosotros de manera satisfactoria trabajos de traducción oficial del inglés al español de documentos técnicos y comerciales propios de nuestra actividad desde el 2008.",
+            texto: "\"Jorge Bermúdez Celín ha realizado para nosotros de manera satisfactoria trabajos de traducción oficial del inglés al español de documentos técnicos y comerciales propios de nuestra actividad desde el 2008.\"",
             firma: [
                 'Carlos Sarmiento Jurado',
                 'Director de Nuevos Productos',
@@ -34,13 +34,13 @@ function About (props) {
             ]
         },
         {
-            texto: "Jorge celebrated my marriage ceremony and translated parts of the ceremony since both of our families come from different language speaking countries. His job was great and added a personal touch to his words. I will always remember this special moment of our lives!",
+            texto: "\"Jorge celebrated my marriage ceremony and translated parts of the ceremony since both of our families come from different language speaking countries. His job was great and added a personal touch to his words. I will always remember this special moment of our lives!\"",
             firma: [
                 'Cynthia Deignan.'
             ]
         },
         {
-            texto: "Jorge Bermúdez Celín presta servicios de traducción e interpretación en nuestra empresa desde hace aproximadamente cinco años. Durante ese tiempo ha demostrado seriedad y cumplimiento con sus obligaciones.",
+            texto: "\"Jorge Bermúdez Celín presta servicios de traducción e interpretación en nuestra empresa desde hace aproximadamente cinco años. Durante ese tiempo ha demostrado seriedad y cumplimiento con sus obligaciones.\"",
             firma: [
                 'Carlos Rafael Casadiego',
                 'Gerente',
@@ -48,7 +48,7 @@ function About (props) {
             ]
         },
         {
-            texto: "Jorge Bermúdez Celin realizó la traducción escrita del inglés al español del documento \"Parrish & Co. (1920) Parrish & Co. Report. Des Moines: Floyd Weirick, R.”, el cual dió origen al barrio El Prado de Barranquilla. El trabajo realizado fue de excelente calidad, dada su experiencia profesional como Planificador Urbano y Traductor Oficial, una coincidencia afortunada para nosotros en la Universidad del Norte.",
+            texto: "\"Jorge Bermúdez Celin realizó la traducción escrita del inglés al español del documento \"Parrish & Co. (1920) Parrish & Co. Report. Des Moines: Floyd Weirick, R.”, el cual dió origen al barrio El Prado de Barranquilla. El trabajo realizado fue de excelente calidad, dada su experiencia profesional como Planificador Urbano y Traductor Oficial, una coincidencia afortunada para nosotros en la Universidad del Norte.\"",
             firma: [
                 'Yalmar Vargas Tovar',
                 'Profesor Tiempo Completo',
@@ -56,7 +56,7 @@ function About (props) {
             ]
         },
         {
-            texto: "Jorge Bermúdez Celín nos ha prestado servicios de traducción simultánea y consecutiva desde el 2019 en diferentes cursos de capacitación dictados en las ciudades de Barranquilla, Santa Marta y Cartagena. Los cursos dictados están relacionados con temas de desarrollo y regionalización portuaria, gestión de cadenas de suministro, logística y transporte fluvial, y operación  de equipos portuarios. El desempeño del señor Bermúdez ha sido excelente, y esta calificación ha sido otorgada no sólo por nosotros, sino por instructores del SENA también.",
+            texto: "\"Jorge Bermúdez Celín nos ha prestado servicios de traducción simultánea y consecutiva desde el 2019 en diferentes cursos de capacitación dictados en las ciudades de Barranquilla, Santa Marta y Cartagena. Los cursos dictados están relacionados con temas de desarrollo y regionalización portuaria, gestión de cadenas de suministro, logística y transporte fluvial, y operación  de equipos portuarios. El desempeño del señor Bermúdez ha sido excelente, y esta calificación ha sido otorgada no sólo por nosotros, sino por instructores del SENA también.\"",
             firma: [
                 'Silvina Pereira Marques',
                 'Area Maneger Latinoamérica',
@@ -64,7 +64,7 @@ function About (props) {
             ]
         },
         {
-            texto: "When I was married in Santo Domingo in 1998, Jorge assisted me first by translating a printed program that I had prepared in English. The program included lyrics to songs that presented a unique challenge to maintain the spirit of the music and relevance to the wedding.  Then as Best Man at the wedding, Jorge ably provided live translation for speeches and other interactions between guests, many of whom spoke only English or Spanish.  Both his editorial and live translations were excellent and contributed significantly to the bi-lingual wedding.",
+            texto: "\"When I was married in Santo Domingo in 1998, Jorge assisted me first by translating a printed program that I had prepared in English. The program included lyrics to songs that presented a unique challenge to maintain the spirit of the music and relevance to the wedding.  Then as Best Man at the wedding, Jorge ably provided live translation for speeches and other interactions between guests, many of whom spoke only English or Spanish.  Both his editorial and live translations were excellent and contributed significantly to the bi-lingual wedding.\"",
             firma: [
                 ' Mark C. Walker.'
             ]
@@ -80,16 +80,19 @@ function About (props) {
                         duration={30}
                     />
                 </h4>
-            
-            <Row className = 'mt-2'>
-                {testimonios.map((testimonio, index) => (
-                    <Col key={index} sm={12} md={6} lg={4} className='testimonios-col shadow rounded p-3'>
-                        <div>
-                            <p className='text-center font-italic'>{testimonio.texto}</p>
-                            {testimonio.firma.map((firma,index) => (<h6 className='text-center font-weight-bold' key={index}>{firma}</h6>))}
-                        </div>
-                    </Col>
-                ))}
+            <Row className = 'my-2'>       
+                <Col sm={12} md={12} lg={12}>
+                    <Carousel className = 'shadow rounded p-3'>
+                        {testimonios.map((testimonio, index) => (
+                            <Carousel.Item className = 'testimonio'>
+                                <Carousel.Caption>
+                                    <h4 className ='text-center font-italic'>{testimonio.texto}</h4>
+                                    {testimonio.firma.map((firma,index) => (<h6 className='text-center font-weight-bold m-0 p-0' key={index}>{firma}</h6>))}
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        ))}
+                    </Carousel>
+                </Col>
             </Row>
             <p className = 'my-5 about-description'>Nuestros clientes son tanto empresas como personas naturales, privados como públicos, compañías locales como multinacionales y en varios sectores, tales como: ingeniería, tecnología, educación, planeación, medicina, salud, legal, financiero y mercadeo.</p>
             <Row>
